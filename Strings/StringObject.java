a963476ad930764d0ee0f15e349958b5bfea98da
Kaushik216198 <2420030004@klh.edu.in>
2025-08-09 11:03:52 +0530

    Successfully uploaded strings question

---
+++ Strings/StringObject.java
@@ -0,0 +1,11 @@
+package Strings;
+import java.util.Scanner;
+public class StringObject {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String str = new String("Hi! This is Kaushik.");
+				System.out.println(str);
+
+	}
+}
